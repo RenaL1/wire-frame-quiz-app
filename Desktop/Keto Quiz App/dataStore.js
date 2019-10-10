e3bb8372c6f3466703b21ae23155df7caaad8239
Rena Lorkis <rlorkis1@gmail.com>
2019-10-10 16:37:43 -0400

    1st round quiz app built in repl

---
+++ Desktop/Keto Quiz App/dataStore.js
@@ -0,0 +1,61 @@
+const STORE = [
+  { 
+    question: 'What is the foundation of a ketogonic diet?',
+    answers:
+     [
+      '20 percent fat, 60 percent protein, 20 percent carbs',
+      '20 percent fat, 60 percent protein, 20 percent carbs',
+      '0 percent proten, 15 percent protien, 85 percent carbs',
+      '75 percnt fat, 25 percent protien, 5 percent carbs',
+     ],
+      correctAnswer:'75 percnt fat, 25 percent protien, 5 percent carbs',
+     },
+
+  {
+     question: 'The ketogenic diet was first introduced as a treatment for ...?',
+     answers:
+     [
+      'Parkinsons',
+      'PCOS',
+      'Migraines',
+      'Epilepsy',
+     ],
+     correctAnswer:'Epilepsy'
+  },
+
+  {
+    question: 'Which one of the following statements about exercise is correct?',
+    answers:
+    [
+    'You cannot outrun a bad diet',
+    'You should always fast before your workouts',
+    'You need to exercise at least 5 times per week',
+    'All of the above',
+    ],
+    correctAnswer:'You cannot outrun a bad diet',
+  },
+
+{
+question:'Which one of the following fats is not recommended for a healthy keto diet?',
+ answers:
+ [
+   'Vegetable oils',
+   'Lard',
+   'Coconut oil',
+   'Butter',
+ ],
+   correctAnswer:'Vegetable oils',
+  },
+
+ {
+question:'Which one of the following statements iscorrect?',
+answers: 
+[
+  'Your ketone leYou always needs to be at least 2 mM for optimal fat burning',
+  'You should eat more fat to enter ketosis',
+  'You need to moniter your hormonal levels',
+  'Sufficient protein is important for inducing satiety',
+],
+correctAnswer:'Sufficient protein is important for inducing satiety'
+ }
+];
